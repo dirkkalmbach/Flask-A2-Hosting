c3f9c36be9b19da6ed46c437402090858fdabb21
dirkkalmbach <dirk.kalmbach@gmail.com>
2020-08-26 10:35:08 +0800

    feat: config.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,20 @@
-from flask import Flask
+# Standard library imports
+import os
+import sys
 import datetime
+
+# Third party imports  
+from flask import Flask, request, render_template, redirect, url_for, flash
+
+# Local application importsimport sys
+from pathlib import Path
+sys.path.append(str(Path('.').absolute().parent))
+
+from config import BaseConfig, DevelopmentConfig
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	return "{} <h1>Hello Planet from A2 </h1>".format(datetime.datetime.now())
+
